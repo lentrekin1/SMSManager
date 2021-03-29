3666a8a33b115e10df62dd28dcacfca46c8b9e28
qazywazy <lancee478@gmail.com>
2021-03-28 18:22:08 -0700

    basic flask/twilio implementation started

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
-
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/sms/msg_recv', methods=['POST'])
+def msg_recv():
+    resp = MessagingResponse()
+    resp.message('test msg 1')
+    return str(resp)
+
+
+if __name__ == '__main__':
+    app.run(port=80)
